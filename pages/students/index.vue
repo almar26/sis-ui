7a3fd014fb7350f1795e4b7b2e56f808aa0221eb
almar26 <gomezalmar26@gmail.com>
2025-02-10 16:24:00 +0800

    update add student dialog box when closing

--- pages/students/index.vue
+++ pages/students/index.vue
@@ -1,64 +1,38 @@
 <template>
   <div>
-    <BaseBreadcrumb
-      :title="page.title"
-      :icon="page.icon"
-      :breadcrumbs="breadcrumbs"
-    ></BaseBreadcrumb>
+    <BaseBreadcrumb :title="page.title" :icon="page.icon" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
     <!-- <h1>{{ userData }}</h1> -->
     <v-row>
       <v-col>
-        
+
         <v-card elevation="0">
           <v-card-title class="d-flex align-center pe-2">
-            <v-btn class="mb-3 text-capitalize" color="primary" prepend-icon="mdi-plus" @click="openCreateDialog()"
-          >Create</v-btn
-        >
+            <v-btn class="mb-3 text-capitalize" color="primary" prepend-icon="mdi-plus"
+              @click="openCreateDialog()">Create</v-btn>
             <v-spacer></v-spacer>
-          <!-- {{ active_sy.semester }} - {{ active_sy.school_year }} -->
+            <!-- {{ active_sy.semester }} - {{ active_sy.school_year }} -->
             <v-spacer></v-spacer>
 
-            <v-text-field
-              v-model="search"
-              density="compact"
-              label="Search"
-              prepend-inner-icon="mdi-magnify"
-              variant="solo-filled"
-              flat
-              hide-details
-              single-line
-            ></v-text-field>
+            <v-text-field v-model="search" density="compact" label="Search" prepend-inner-icon="mdi-magnify"
+              variant="solo-filled" flat hide-details single-line></v-text-field>
           </v-card-title>
 
           <v-divider></v-divider>
-          <v-data-table
-            density="compact"
-            :search="search"
-            :items="studentList"
-            :headers="headers"
-            :loading="loading"
-            items-per-page="20"
-          >
+          <v-data-table density="compact" :search="search" :items="studentList" :headers="headers" :loading="loading"
+            items-per-page="20">
             <template v-slot:loading>
               <v-skeleton-loader type="table-row@10"></v-skeleton-loader>
             </template>
             <template v-slot:[`item.sy`]="{ item }">
               <!-- {{ item.school_year_start }}-{{ item.school_year_end }} -->
-                {{  item.school_year }}
+              {{ item.school_year }}
             </template>
 
             <template v-slot:[`item.actions`]="{ item }">
               <v-tooltip text="View Profile" location="top">
                 <template v-slot:activator="{ props }">
-                  <v-btn
-                    size="medium"
-                    :to="`/students/${item.documentId}`"
-                    class="mr-1"
-                    variant="text"
-                    v-bind="props"
-                    icon="mdi-open-in-new"
-                    color="green"
-                  >
+                  <v-btn size="medium" :to="`/students/${item.documentId}`" class="mr-1" variant="text" v-bind="props"
+                    icon="mdi-open-in-new" color="green">
                   </v-btn>
                 </template>
               </v-tooltip>
@@ -99,14 +73,8 @@
             <v-row dense>
               <v-col cols="12" md="4" sm="6">
                 <!-- <label class="label text-grey-darken-2" for="email">Course</label> -->
-                <v-select
-                  :items="semesterList"
-                  label="Semester*"
-                  v-model="semester"
-                  :rules="rules.semester"
-                  variant="outlined"
-                  required
-                ></v-select>
+                <v-select :items="semesterList" label="Semester*" v-model="semester" :rules="rules.semester"
+                  variant="outlined" required></v-select>
               </v-col>
               <v-col cols="12" md="2"></v-col>
               <v-col cols="12" md="2"></v-col>
@@ -133,37 +101,20 @@
               </v-col> -->
 
               <v-col cols="12" md="4" sm="6">
-                <v-text-field label="School Year*" :rules="rules.schoolyear" v-model="schoolYear"
-                 variant="outlined" required></v-text-field>
+                <v-text-field label="School Year*" :rules="rules.schoolyear" v-model="schoolYear" variant="outlined"
+                  required></v-text-field>
               </v-col>
             </v-row>
             <v-row dense>
               <v-col cols="12" md="4" sm="6">
                 <!-- <label class="label mb-4" for="email">Student No</label> -->
-                <v-text-field
-                  color="primary"
-                  label="Student No.*"
-                  :rules="rules.studentno"
-                  v-model="studentno"
-                  variant="outlined"
-                  retu
-                  required
-                ></v-text-field>
+                <v-text-field color="primary" label="Student No.*" :rules="rules.studentno" v-model="studentno"
+                  variant="outlined" retu required></v-text-field>
               </v-col>
               <v-col cols="12" md="8" sm="6">
                 <!-- <label class="label text-grey-darken-2" for="email">Course</label> -->
-                <v-select
-                  color="primary"
-                  :items="courses"
-                  item-title="code"
-                  item-value="code"
-                  label="Course*"
-                  v-model="course"
-                  :rules="rules.course"
-                  variant="outlined"
-                  return-object
-                  required
-                ></v-select>
+                <v-select color="primary" :items="courses" item-title="code" item-value="code" label="Course*"
+                  v-model="course" :rules="rules.course" variant="outlined" return-object required></v-select>
                 <!-- <v-select
                   v-if="selectedBA"
                   color="primary"
@@ -174,75 +125,37 @@
                   v-model="major"
                   variant="outlined"
                 ></v-select> -->
-                <v-text-field v-if="selectedBA"
-                color="primary"
-                label="Major"
-                  v-model="major"
-                  variant="outlined"
-                >
-                  
+                <v-text-field v-if="selectedBA" color="primary" label="Major" v-model="major" variant="outlined">
+
                 </v-text-field>
               </v-col>
             </v-row>
             <v-row dense>
               <v-col cols="12" md="4" sm="6">
-                <v-text-field
-                  color="primary"
-                  v-model="lastname"
-                  :rules="rules.lastname"
-                  variant="outlined"
-                  label="Last name*"
-                  @input="lastname = lastname.toUpperCase()"
-                ></v-text-field>
+                <v-text-field color="primary" v-model="lastname" :rules="rules.lastname" variant="outlined"
+                  label="Last name*" @input="lastname = lastname.toUpperCase()"></v-text-field>
               </v-col>
 
               <v-col cols="12" md="4" sm="6">
-                <v-text-field
-                  color="primary"
-                  label="First name*"
-                  :rules="rules.firstname"
-                  v-model="firstname"
-                  variant="outlined"
-                  persistent-hint
-                  @input="firstname = firstname.toUpperCase()"
-                  required
-                ></v-text-field>
+                <v-text-field color="primary" label="First name*" :rules="rules.firstname" v-model="firstname"
+                  variant="outlined" persistent-hint @input="firstname = firstname.toUpperCase()"
+                  required></v-text-field>
               </v-col>
               <v-col cols="12" md="4" sm="6">
-                <v-text-field
-                  color="primary"
-                  label="Middle name"
-                  v-model="middlename"
-                  variant="outlined"
-                  persistent-hint
-                  @input="middlename = middlename.toUpperCase()"
-                  required
-                ></v-text-field>
+                <v-text-field color="primary" label="Middle name" v-model="middlename" variant="outlined"
+                  persistent-hint @input="middlename = middlename.toUpperCase()" required></v-text-field>
               </v-col>
             </v-row>
             <v-row dense>
               <v-col>
-                <v-textarea
-                  v-model="address"
-                  color="primary"
-                  label="Address"
-                  rows="1"
-                  variant="outlined"
-                  @input="address = address.toUpperCase()"
-                  auto-grow
-                ></v-textarea>
+                <v-textarea v-model="address" color="primary" label="Address" rows="1" variant="outlined"
+                  @input="address = address.toUpperCase()" auto-grow></v-textarea>
               </v-col>
             </v-row>
             <v-row dense>
               <v-col cols="12" md="4" sm="6">
-                <v-select
-                  color="primary"
-                  v-model="gender"
-                  :items="['MALE', 'FEMALE']"
-                  variant="outlined"
-                  label="Gender"
-                  required
-                ></v-select>
+                <v-select color="primary" v-model="gender" :items="['MALE', 'FEMALE']" variant="outlined" label="Gender"
+                  required></v-select>
 
                 <!-- <v-select v-model="selected" :items="courses" :item-title="courses" >
                   </v-select> -->
@@ -250,30 +163,15 @@
                   v-model="course" :rules="rules.course" variant="outlined" return-object required></v-select> -->
               </v-col>
               <v-col cols="12" md="4" sm="6">
-                <date-picker
-                  v-model="bday"
-                  clearable
-                  color="primary"
-                  variant="outlined"
-                  label="Birthday"
-                ></date-picker>
+                <date-picker v-model="bday" clearable color="primary" variant="outlined" label="Birthday"></date-picker>
               </v-col>
               <v-col cols="12" md="4" sm="6">
-                <v-text-field
-                  color="primary"
-                  label="Age"
-                  v-model="age"
-                  variant="outlined"
-                  persistent-hint
-                  required
-                  type="number"
-                ></v-text-field>
+                <v-text-field color="primary" label="Age" v-model="age" variant="outlined" persistent-hint required
+                  type="number"></v-text-field>
               </v-col>
             </v-row>
 
-            <small class="text-caption text-medium-emphasis"
-              >*indicates required field</small
-            >
+            <small class="text-caption text-medium-emphasis">*indicates required field</small>
           </v-form>
         </v-card-text>
 
@@ -284,15 +182,8 @@
 
           <!-- <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn> -->
 
-          <v-btn
-            color="green"
-            text="Save"
-            variant="flat"
-            width="140"
-            :loading="loadingCreateStudent"
-            @click="createStudent()"
-            prepend-icon="mdi-content-save"
-          ></v-btn>
+          <v-btn color="green" text="Save" variant="flat" width="140" :loading="loadingCreateStudent"
+            @click="createStudent()" prepend-icon="mdi-content-save"></v-btn>
           <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
@@ -405,8 +296,8 @@ const rules = ref({
 
 // Open Create Student Dialog box
 async function openCreateDialog() {
-dialog.value = true;
-getCoursesList()
+  dialog.value = true;
+  getCoursesList()
 }
 
 async function createStudent() {
@@ -445,6 +336,8 @@ async function createStudent() {
         //dialog.value = false;
         loadingCreateStudent.value = false;
         createStudentForm.value?.reset();
+        semester.value = active_sy.value?.semester
+        schoolYear.value = active_sy.value?.school_year
         age.value = 0
         toast.success("Successfully created!");
         initialize();
@@ -515,6 +408,8 @@ watch([course, bday, dialog], async () => {
   if (dialog.value == false) {
     console.log("Create student dialog box closed");
     createStudentForm.value?.reset();
+    semester.value = active_sy.value?.semester
+    schoolYear.value = active_sy.value?.school_year
     loadingCreateStudent.value = false;
     bday.value = null;
   }
@@ -522,7 +417,7 @@ watch([course, bday, dialog], async () => {
 
 onMounted(async () => {
   await initialize();
-    //console.log("age: ", +getAge("1990/10/08"));
+  //console.log("age: ", +getAge("1990/10/08"));
 });
 </script>
 
@@ -537,25 +432,24 @@ onMounted(async () => {
   border-bottom-right-radius: inherit;
 }
 
-:deep() .v-table .v-table__wrapper > table > thead > tr > th {
+:deep() .v-table .v-table__wrapper>table>thead>tr>th {
   border-right: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
-  border-bottom: thick solid
-    rgba(var(--v-border-color), var(--v-border-opacity));
+  border-bottom: thick solid rgba(var(--v-border-color), var(--v-border-opacity));
   font-weight: bold;
   /* background-color: #04aa6d;
   color: white; */
 }
 
-:deep() .v-table .v-table__wrapper > table > tbody > tr > td:not(:last-child),
-.v-table .v-table__wrapper > table > tbody > tr > th:not(:last-child) {
+:deep() .v-table .v-table__wrapper>table>tbody>tr>td:not(:last-child),
+.v-table .v-table__wrapper>table>tbody>tr>th:not(:last-child) {
   border-right: thin solid rgba(var(--v-border-color), var(--v-border-opacity));
 }
 
-:deep() .v-table .v-table__wrapper > table > tbody > tr:nth-child(even) {
+:deep() .v-table .v-table__wrapper>table>tbody>tr:nth-child(even) {
   background-color: #f2f2f2;
 }
 
-:deep() .v-table .v-table__wrapper > table > tbody > tr:hover {
+:deep() .v-table .v-table__wrapper>table>tbody>tr:hover {
   background-color: #E8F5E9;
 }
 </style>
